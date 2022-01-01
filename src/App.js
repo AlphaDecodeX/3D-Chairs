233549537cb7b6c4b93aa087fdc9ae3624f1c43e
unknown <lpsk5713@gmail.com>
2022-01-02 05:15:32 +0530

    Project created with inbuilt models and smooth effect

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-import React from "react";
+import React from 'react'
+import { Canvas } from 'react-three-fiber'
+import Header from './components/header'
+import { Html } from 'drei'
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Canvas
+                colorManagement
+                camera={{ position: [0, 0, 120], fov: 70 }}
+            >
+                <Html fullscreen>
+                    <div className="container">
+                        <div className="title">
+                            Hello
+                        </div>
+                    </div>
+                </Html>
+            </Canvas>
+        </>
+    )
+}
+
+export default App
